6b9d0f5649a394732b2747bc425727bb55972cca
newancroos <newmancroos@gmail.com>
2025-03-06 20:34:09 -0500

    Completed Api and Client and Api from Cliant

---
+++ Shopping/Shopping.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopping.API.Models;
+
+public class Product
+{
+    public string Id { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string Category { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+}
